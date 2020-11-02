3b0443ab172826eb98280f562eefcea441afb415
missipboy89 <missipboy89@gmail.com>
2020-11-01 18:45:32 -0600

    test publish

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
     <div>
         <b-navbar toggleable="lg" type="dark" variant="dark" sticky>
-            <b-navbar-brand tag="h1" class="platoon" to="/">FRONTLINE SOC</b-navbar-brand>
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
             <b-collapse id="nav-collapse" is-nav>
                 <b-navbar-nav>

ada92471f2137808b597d1ace47a0d494a4647e1
missipboy89 <missipboy89@gmail.com>
2020-11-05 17:47:15 -0600

    navbar brand

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,55 +1,6 @@
 <template>
     <div>
         <b-img :src="require('../assets/unnamed.jpg')" fluid-grow></b-img>
-        <b-container fluid class="card">
-            <b-card-group deck>
-                <b-card
-                    title="Upcoming Events"
-                    :img-src="require('../assets/cross.jpg')"
-                    img-alt="cross"
-                    img-top
-                    tag="article"
-                    style="max-width: 20rem;"
-                    class="mb-2 card2"
-                >
-                    <b-card-text>
-                        Keep an eye open for our latest events to come.
-                    </b-card-text>
-
-                    <b-button to="/calendar" variant="primary">More info</b-button>
-                </b-card>
-                <b-card
-                    title="Who We Are"
-                    :img-src="require('../assets/cross.jpg')"
-                    img-alt="cross"
-                    img-top
-                    tag="article"
-                    style="max-width: 20rem;"
-                    class="mb-2 card2"
-                >
-                    <b-card-text>
-                        Get to know uu! Learn about where we comefrom and God uses us on the Frontline.
-                    </b-card-text>
-
-                    <b-button to="/about" variant="primary">More info</b-button>
-                </b-card>
-                <b-card
-                    title="Hear From Us"
-                    :img-src="require('../assets/cross.jpg')"
-                    img-alt="cross"
-                    img-top
-                    tag="article"
-                    style="max-width: 20rem;"
-                    class="mb-2 card2"
-                >
-                    <b-card-text>
-                        Reach out to us and join our mailing list, or just let us know what you think.
-                    </b-card-text>
-
-                    <b-button to="/contact" variant="primary">More info</b-button>
-                </b-card>
-            </b-card-group>
-        </b-container>
     </div>
 </template>
 
@@ -60,17 +11,5 @@ export default {
 </script>
 
 <style scoped>
-* {
-    box-sizing: border-box;
-}
-.card {
-    float: left;
-}
-div {
-    background-color: lightgray;
-}
-.test {
-    margin-left: 400px;
-    padding: 10px;
-}
+
 </style>

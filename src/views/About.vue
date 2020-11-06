ed9936012ff8b1e7382da4efa236c3334cdecc3a
missipboy89 <missipboy89@gmail.com>
2020-11-05 19:41:19 -0600

    home 1.0

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,90 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <b-card
+            overlay
+            text-variant="white"
+            :img-src="require('../assets/18160.jpg')"
+            img-alt="Card Image"
+            align="center"
+        >
+            <b-card-text class="abt">
+                ABOUT US
+            </b-card-text>
+        </b-card>
   </div>
 </template>
+
+<script>
+export default {
+    name: 'About'
+}
+</script>
+
+<style scoped>
+@font-face {
+  font-family: PLATOoN;
+  src: local("PLATOoN"),
+  url(../fonts/PLATOoN.ttf) format("truetype");
+}
+* { margin: 0; }
+
+@media only screen and (max-width: 600px) {
+.abt {
+    font-family: "PLATOoN";
+    font-size: 60px;
+    color: black;
+    text-align: center;
+    margin: auto;
+    padding: 10px;
+    text-shadow: 2px 2px 20px white;
+    }
+}
+@media only screen and (min-width: 600px) {
+.abt {
+    font-family: "PLATOoN";
+    font-size: 80px;
+    color: black;
+    text-align: center;
+    margin: auto;
+    padding: 10px;
+    text-shadow: 2px 2px 20px white;
+    }
+}
+@media only screen and (min-width: 768px) {
+.abt {
+    font-family: "PLATOoN";
+    font-size: 100px;
+    color: black;
+    text-align: center;
+    margin: auto;
+    padding: 50px;
+    text-shadow: 2px 2px 20px white;
+    }
+}
+@media only screen and (min-width: 992px) {
+.abt {
+    font-family: "PLATOoN";
+    font-size: 130px;
+    color: black;
+    text-align: center;
+    margin: auto;
+    padding: 50px;
+    text-shadow: 2px 2px 20px white;
+    }
+}
+@media only screen and (min-width: 1200px) {
+.abt {
+    font-family: "PLATOoN";
+    font-size: 180px;
+    color: black;
+    text-align: center;
+    margin: auto;
+    padding: 70px;
+    text-shadow: 2px 2px 20px white;
+    }
+img {
+    max-height: 500px;
+    width: 100%;
+}
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,53 @@
 <template>
     <div>
-        <b-img :src="require('../assets/unnamed.jpg')" fluid-grow></b-img>
+        <b-img :src="require('../assets/unnamed2.jpg')" fluid-grow></b-img>
+        <div>
+            <b-container fluid class="p-3 bg-dark">
+                <b-card-group deck>
+                    <b-card title="Upcoming Events"
+                    :img-src="require('../assets/cal.jpg')"
+                    img-alt="cross"
+                    img-top
+                    >
+                        <b-card-text>
+                            <h6>Keep up to date on our upcoming classes and events.</h6>
+                        </b-card-text>
+                        <template #footer>
+                            <b-button variant="primary" to="/calendar">Calendar</b-button>
+                        </template>
+                    </b-card>
+                    <b-card title="Our Mission"
+                    :img-src="require('../assets/cross.jpg')"
+                    img-alt="cross"
+                    img-top
+                    >
+                        <b-card-text>
+                            <h6>This is who we are and where we stand. Find out more about us.</h6>
+                        </b-card-text>
+                        <template #footer>
+                            <b-button variant="primary" to="/about">About Us</b-button>
+                        </template>
+                    </b-card>
+                    <b-card title="Upcoming Events"
+                    :img-src="require('../assets/cross.jpg')"
+                    img-alt="cross"
+                    img-top
+                    >
+                        <b-card-text>
+                            Keep up to date on our upcoming classes and events.
+                        </b-card-text>
+                        <template #footer>
+                            <b-button variant="primary" to="/contact">Contact Us</b-button>
+                        </template>
+                    </b-card>
+                </b-card-group>
+            </b-container>
+            <b-container fluid class="p-5 bg-dark">
+                <b-row>
+                    <b-img rounded :src="require('../assets/tim.jpg')" fluid-grow alt="Responsive image"></b-img>
+                </b-row>
+            </b-container>
+        </div>
     </div>
 </template>
 
